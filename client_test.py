2d1f28e309aa5ada963238aba5ebcc3583be0f2f
2105789 <suvoohmag@gmail.com>
2024-09-08 00:18:36 +0530

    Task 1

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,30 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_priceBZero(self):
+    price_a = 120.48
+    price_b = 0
+    self.assertIsNone(getRatio(price_a, price_b), "Expected None when price_b is zero")
+
+  def test_getRatio_greaterThan1(self):
+    price_a = 150
+    price_b = 100
+    self.assertGreater(getRatio(price_a, price_b), 1)
+
+  def test_getRatio_lessThan1(self):
+    price_a = 100
+    price_b = 150
+    self.assertLess(getRatio(price_a, price_b), 1)
+
+  def test_getRatio_equalTo1(self):
+    price_a = 150
+    price_b = 150
+    self.assertEqual(getRatio(price_a, price_b), 1)
 
 
 
